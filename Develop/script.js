fe54269267c56a3396035d0a8b9f6a875644505c
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2022-11-09 08:27:16 -0400

    added comments in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//generate password function to get the password
+//prompt for password criteria
 function generatePassword() {
   var userInput = window.prompt("What is the desired length of the password to be? The password should be between 8 and 128.")
   console.log(userInput)
@@ -19,20 +20,21 @@ function generatePassword() {
     alert("Please choose between 8 and 128.")
     return
   }
-
+  // characters type variable
   var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
   var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
   var numberic = ["0123456789"]
   var specialCharacters = ["~!@#$%^&*()"]
 
-
+  //confirmation for character type
   var userChooseLowerCase = confirm("Would you like to include the lowercase?")
   var userChooseUppCase = confirm("Would you like to include the uppercase?")
   var userChooseNum = confirm("Would you like to include the number?")
   var userChooseSpecial = confirm("Would you like to include the special characters?")
 
 
-
+  //conditional statement for one character type must be selected
+  //conditional statement for slected password criteria
   if (userChooseLowerCase || userChooseNum || userChooseSpecial || userChooseUppCase) {
     alert("Thank you")
   } else {
@@ -61,17 +63,15 @@ function generatePassword() {
   }
   console.log(desiredPassword);
 
-
+  //variable for the final password
   var password = [];
-
+  // generate password
   for (var i = 1; i <= userInput; i++) {
     var randomPw = Math.floor(Math.random() * desiredPassword.length + 1);
     password += desiredPassword.charAt(randomPw)
   }
   return password;
 }
-
-
 
 
 // Write password to the #password input
